4b3be6a8be62214c1c0df30c8ac89a5c3063344f
gizem doğan <77739683+gizdoan@users.noreply.github.com>
2021-02-18 21:23:23 +0200

    Add files via upload

---
+++ Metodlar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------------");
+                
+            }
+
+            Console.WriteLine("--------Metodlar---------");
+
+            //instance - örnek
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 8);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 14);
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+//Don't repead yourself - DRY - Clean Code - Best Practice
